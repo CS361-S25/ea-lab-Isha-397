075eb374d174a534a862b79759ea1d5521aa9d1f
Isha Patel <ishapatel@Ishas-MacBook-Air-2.local>
2025-04-04 10:27:37 -0500

    working on my lab

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include "Empirical/include/emp/math/Random.hpp"
 
-int main() {
+class Organism {
+    public: 
+        double behavior;
+        
+        Organism (double b) : behavior(b) {};
+};
 
+int main() {
+    emp:: vector<Orgamism> population;
+
+    for (int i = o, i < 100, i++) {
+        std :: cout << "Behavior of Organism 0:" << population[0].behavior << std::endl;
+    }
+
+    
 }
