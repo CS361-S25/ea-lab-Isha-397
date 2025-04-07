13e583ec03e0c806ed7179254157814d3029086e
Isha Patel <ishapatel@Ishas-MacBook-Air-2.local>
2025-04-07 09:31:38 -0500

    working on my lab

--- main.cpp
+++ main.cpp
@@ -7,18 +7,29 @@ class Organism {
         
         Organism (double b) : behavior(b) {};
 
-        Organism* reproduce() {
-            return new Organism(this->behavior);
+        void mutate(emp::Random random) {
+            behavior += random.GetRandNormal(0.0, 0.02);
+            // random.GetRandNormal(0.0, 0.02);
         }
-    
+
+        Organism* reproduce() {
+            // return new Organism(this->behavior);
+            Organism* offspring = new Organism(behavior);
+            offspring->mutate(random);
+            return offspring;
+        }
+
+        
 };
 
 int main() {
     emp:: vector<Organism> population;
+    emp:: Random random = *new emp::Random(1);
 
     // initializing populaiton with 100 organisms with the same behavior
     for (int i = 0; i < 100; i++) {
         population.push_back(*new Organism(0.5));
+        population[i].mutate[random]
         //std :: cout << "Behavior of Organism 9:" << population[9].behavior << std::endl;
     }
 
@@ -47,8 +58,23 @@ int main() {
         population[0] = *new_org; 
 
         delete new_org;
+
     }
 
-    std :: cout << "population size after updates " << population.size() << std::endl;
+    // std :: cout << "population size after updates " << population.size() << std::endl;
     
+
+    Organism* best = &population[0];
+    for (Organism& org : population) {
+        if (org.behavior > best->behavior) {
+            best = &org;
+        }
+    }
+
+    std::cout << "Max behavior after evolution: " << best->behavior << std::endl;
+    std::cout << "Final population size: " << population.size() << std::endl;
+
+    return 0;
+    
+
 }
