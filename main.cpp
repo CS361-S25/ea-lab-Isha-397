4b093027de7633ab36a0ad0b7e897b8f50555745
Isha Patel <ishapatel@Ishas-MacBook-Air-2.local>
2025-04-10 16:55:03 -0500

    working on my lab

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ class Organism {
     public: 
         double behavior;
         
-        Organism (double b) : behavior(b) {};
+        Organism (double b = 0 ) : behavior(b) {};
 
         void mutate(emp::Random random) {
             behavior += random.GetRandNormal(0.0, 0.02);
             // random.GetRandNormal(0.0, 0.02);
         }
 
-        Organism* reproduce() {
+        Organism* reproduce(emp::Random random) {
             // return new Organism(this->behavior);
             Organism* offspring = new Organism(behavior);
             offspring->mutate(random);
@@ -29,7 +29,7 @@ int main() {
     // initializing populaiton with 100 organisms with the same behavior
     for (int i = 0; i < 100; i++) {
         population.push_back(*new Organism(0.5));
-        population[i].mutate[random]
+        population[i].mutate(random);
         //std :: cout << "Behavior of Organism 9:" << population[9].behavior << std::endl;
     }
 
@@ -46,18 +46,20 @@ int main() {
     //100 updates
     for (int update = 0; update < 100; update++ ){
 
-        Organism* best_org = &population[0]; 
-        for (Organism& org : population) {
-            if(org.behavior > best_org->behavior){
-                best_org = &org;
+        Organism& best_org = population[0]; 
+        for (Organism org : population) {
+            if(org.behavior > best_org.behavior){
+                best_org = org;
             }
         }
 
-        Organism* new_org = best_org->reproduce();
+        Organism* new_org = best_org.reproduce(random);
 
         population[0] = *new_org; 
 
-        delete new_org;
+        size_t overwrite = random.GetUInt(0,100);
+        population[overwrite] = *new_org;
+        // delete new_org;
 
     }
 
@@ -65,7 +67,7 @@ int main() {
     
 
     Organism* best = &population[0];
-    for (Organism& org : population) {
+    for (Organism org : population) {
         if (org.behavior > best->behavior) {
             best = &org;
         }
