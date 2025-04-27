45309c977df54cddc2f61aeb97fbaf3e5c543fd6
Isha Patel <ishapatel@Ishas-MacBook-Air-2.local>
2025-04-27 12:01:29 -0500

    Finished EA LAb

--- main.cpp
+++ main.cpp
@@ -1,82 +1,62 @@
 #include <iostream>
 #include "Empirical/include/emp/math/Random.hpp"
 
-class Organism {
-    public: 
-        double behavior;
-        
-        Organism (double b = 0 ) : behavior(b) {};
+class Creature {
+public:
+    double behavior_value;
 
-        void mutate(emp::Random random) {
-            behavior += random.GetRandNormal(0.0, 0.02);
-            // random.GetRandNormal(0.0, 0.02);
-        }
+    Creature(double b = 0.0) : behavior_value(b) {}
 
-        Organism* reproduce(emp::Random random) {
-            // return new Organism(this->behavior);
-            Organism* offspring = new Organism(behavior);
-            offspring->mutate(random);
-            return offspring;
-        }
+    void Mutate(emp::Random& rng) {
+        behavior_value += rng.GetRandNormal(0.0, 0.02);
+    }
 
-        
+    Creature* MakeOffspring(emp::Random& rng) {
+        Creature* offspring = new Creature(behavior_value);
+        offspring->Mutate(rng);
+        return offspring;
+    }
 };
 
 int main() {
-    emp:: vector<Organism> population;
-    emp:: Random random = *new emp::Random(1);
+    emp::vector<Creature> creatures;
+    emp::Random rng(1);
 
-    // initializing populaiton with 100 organisms with the same behavior
-    for (int i = 0; i < 100; i++) {
-        population.push_back(*new Organism(0.5));
-        population[i].mutate(random);
-        //std :: cout << "Behavior of Organism 9:" << population[9].behavior << std::endl;
+    // Fill the population
+    for (size_t idx = 0; idx < 100; ++idx) {
+        Creature temp(0.5);
+        temp.Mutate(rng);
+        creatures.push_back(temp);
     }
 
-    // Organism* parent = &population[9];
-    // Organism* child = parent->reproduce();
-
-    // std::cout << "Parent address: " << parent << ", Behavior: " << parent->behavior << std::endl;
-    // std::cout << "Child  address: " << child << ", Behavior: " << child->behavior << std::endl;
-
-    // delete child;
-
-    // return 0;
-
-    //100 updates
-    for (int update = 0; update < 100; update++ ){
-
-        Organism& best_org = population[0]; 
-        for (Organism org : population) {
-            if(org.behavior > best_org.behavior){
-                best_org = org;
+    // Evolution over 100 updates
+    for (size_t gen = 0; gen < 100; ++gen) {
+        Creature* top_creature = &creatures[0];
+        for (Creature& c : creatures) {
+            if (c.behavior_value > top_creature->behavior_value) {
+                top_creature = &c;
             }
         }
 
-        Organism* new_org = best_org.reproduce(random);
+        Creature* offspring = top_creature->MakeOffspring(rng);
+        creatures[0] = *offspring;
 
-        population[0] = *new_org; 
+        size_t idx_to_replace = rng.GetUInt(creatures.size());
+        creatures[idx_to_replace] = *offspring;
 
-        size_t overwrite = random.GetUInt(0,100);
-        population[overwrite] = *new_org;
-        // delete new_org;
-
+        delete offspring; // Avoid memory leak
     }
 
-    // std :: cout << "population size after updates " << population.size() << std::endl;
-    
-
-    Organism* best = &population[0];
-    for (Organism org : population) {
-        if (org.behavior > best->behavior) {
-            best = &org;
+    // Find the final best creature
+    Creature* best = &creatures[0];
+    for (Creature& c : creatures) {
+        if (c.behavior_value > best->behavior_value) {
+            best = &c;
         }
     }
 
-    std::cout << "Max behavior after evolution: " << best->behavior << std::endl;
-    std::cout << "Final population size: " << population.size() << std::endl;
+    std::cout << "Highest behavior: " << best->behavior_value << std::endl;
+    std::cout << "Population size: " << creatures.size() << std::endl;
 
     return 0;
-    
-
 }
